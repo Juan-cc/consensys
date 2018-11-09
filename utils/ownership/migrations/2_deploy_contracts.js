b16665416f86aa13bb34c5cd5c8f068590429d58
Juan Castellon <jcc.utn@gmail.com>
2018-11-09 17:46:19 -0600

    Destructible contract added

--- utils/ownership/migrations/2_deploy_contracts.js
+++ utils/ownership/migrations/2_deploy_contracts.js
@@ -1,5 +1,8 @@
 var Ownable = artifacts.require("./Ownable.sol");
+var Destructible = artifacts.require("./Destructible.sol");
 
 module.exports = function(deployer) {
   deployer.deploy(Ownable);
+  deployer.deploy(Destructible);
+
 };
