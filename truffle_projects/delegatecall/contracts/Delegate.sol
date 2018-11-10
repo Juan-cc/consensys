ca1e0c4e98059dfde8cb6b55973cf579152a8339
Juan Castellon <jcc.utn@gmail.com>
2018-11-10 16:36:49 -0600

    Formatting changed

--- truffle_projects/delegatecall/contracts/Delegate.sol
+++ truffle_projects/delegatecall/contracts/Delegate.sol
@@ -1,7 +1,8 @@
 pragma solidity ^0.4.11;
 
 contract Delegate{
-    address public delegateContract; // Although I don't need this value, we need to map Main's storage location so we need to copy all it's members
+    address public delegateContract; // Although I don't need this value, we need 
+    // to map Main's storage location so we need to copy all it's members
     uint256 public total;
 
     function add(uint256 var1, uint256 var2) public {
